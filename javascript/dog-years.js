a69ea8f1933619fe3d29124cd7af2b0d39a76de7
Tyler Sunderland <134873559+ThatTyGuy@users.noreply.github.com>
2023-06-25 14:21:25 -0400

    Create dog-years.js

---
+++ javascript/dog-years.js
@@ -0,0 +1,15 @@
+const myAge = 31;
+// Declaring a new variable that is set at my current age.
+let earlyYears = 2;
+// Declaring a new variable that is set at the amount of years a dog ages at a different rate.
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+// Declaring a new variable that accounts for the 2 early years that age at a different rate.
+laterYears *= 4;
+// Converting the remaining years to dog years.
+const myAgeInDogYears = earlyYears + laterYears;
+// Adding the early years and later years in dog years.
+const myName = 'TYLER'.toLowerCase();
+// Adding my name to a new variable and converting all the characters to lowercase.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+// Printing a statement in the console that consolidates all the information that was inputted in this project.
